656fcf371fcbb1f471b636f1b414e3c229d8e3e3
Tom Nguyen <tomnguyen@Toms-MBP.hitronhub.home>
2021-11-29 00:35:15 -0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://tnguyen252:tnguyen252@cluster0.6mno9.mongodb.net/MyDB_100369156?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://tnguyen252:tnguyen252@cluster0.6mno9.mongodb.net/MyDB_100369156?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
